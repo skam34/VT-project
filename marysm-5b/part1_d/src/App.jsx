f4856248988a804bd45ebfdab5f446fd5b64b31c
Max <maksymilian.marys@gmail.com>
2023-10-12 14:39:02 +0200

    part1_d rozpoczecie (https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps#conditional-rendering)

---
+++ marysm-5b/part1_d/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setLeft(updatedRight)
+    setTotal(updatedRight + right) 
+  }
+
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total: {total}</p>
+    </div>
+  )
+}
+
+export default App
