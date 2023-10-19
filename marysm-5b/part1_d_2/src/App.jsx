32956aaa2e8ead16278389a67d6ad642e4927eb3
Max <maksymilian.marys@gmail.com>
2023-10-19 14:08:59 +0200

    part1_d finished (without exercises)

--- marysm-5b/part1_d_2/src/App.jsx
+++ marysm-5b/part1_d_2/src/App.jsx
@@ -6,17 +6,27 @@ const Button = ({text, handleClick}) => (
   </button>
 )
 
+const Display = props => <div>{props.value}</div>
+
+
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const resetValueHandler = () => {
-    setValue(0);
-  }
+  const setValueHandler = (number) => () => {
+      setValue(number);
+      console.log("Value is set to", number)
+    }
 
   return (
     <div>
-      {value}
-      <Button text="reset to zero" handleClick={resetValueHandler}/>
+      <Display value={value}/>
+      <Button text="Set to 0" handleClick={setValueHandler(0)}/>
+      <Button text="Set to 100" handleClick={setValueHandler(100)}/>
+      <br/>
+      <Button text="Add 1" handleClick={setValueHandler(value+1)}/>
+      <Button text="Take 1" handleClick={setValueHandler(value-1)}/>
+      
+
     </div>
   )
 }
